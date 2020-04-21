d90e58bfc9292b07f1bf4fd7f2ab8a11cb242c8e
Andrei <0710943@gmail.com>
2020-04-21 14:13:43 +1000

    70% key gen

--- Source.cpp
+++ Source.cpp
@@ -1,34 +1,22 @@
 #include "shif.h"
 
 int main() {
-	//
+	replacement a;
+	a.key_generator("my_alphabet.alph", "ssss", "ssss.key");
 	//json jj;
-
+	//fstream file("my_alphabet.alph");
+	//string aa;
+	//getline(file,aa);
+	//cout << aa << endl;
 	//jj["alp"] = json::array({ "A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J", "j", "K", "k", "L", "l", "M", "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r", "S", "s", "T", "t", "U", "u", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z" }); //= "A","a","B","b","C","c","D","d","E","e","F","f","G","g","H","h","I","i","J","j","K","k","L","l","M","m","N","n","O","o","P","p","Q","q","R","r","S","s","T","t","U","u","V","v","W","w","X","x","Y","y","Z","z";
-	//ofstream file("my_alphabet.alph");
-	//file << jj;
-	//cout << jj;
-
-
-
-	//ifstream file("aaa.txt");
-	//file >> RecentFiles;
-	//for (const auto& item : RecentFiles.items())
-	//{
-	//	std::cout << item.key() << "\n";
-	//	for (const auto& val : item.value().items())
-	//	{
-	//		std::cout << "  " << val.key() << ": " << val.value() << "\n";
-	//	}
+	//
+	//json jjj = jj.at("/alp"_json_pointer);
+	//cout << jjj;
+	//json::iterator it = jjj.begin();
+	//for (int i = 0; i < jjj.size(); i++) {
+	//	cout << it[i] << endl;
+	//	
 	//}
-	//std::cout << "\nor\n\n";
-	//for (const auto& item : RecentFiles.items())
-	//{
-	//	std::cout << item.key() << "\n";
-	//	std::cout << "  " << item.value()["Name"].get<std::string>() << "\n";
-	//	std::cout << "  " << item.value()["Last modified"].get<std::string>() << "\n";
-	//	std::cout << "  " << item.value()["Score"].get<double>() << "\n";
-	//}
-	//file.close();
+	
 	return 0;
 }
--- shif.h
+++ shif.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <ctime>
 #include "nlohmann/json.hpp"
 using namespace std;
 using json = nlohmann::json;
@@ -15,13 +16,60 @@ private:
 	bool FileIsExist(string filePath)
 	{
 		bool isExist = false;
-		ifstream fin(filePath.c_str());
+		ifstream fin(filePath);
 
-		if (fin.is_open())
+		if (fin.is_open()) {
+
+		}
+		else {
 			isExist = true;
+		}
 
 		fin.close();
 		return isExist;
+	}
+	int black_list_check(string bl, int chack) {
+		bool trriger = 0;
+		for (int i = 0; i < bl.size(); i++) {
+			if (bl[i] == chack) {
+				trriger = 1;
+				break;
+			}
+		}
+		if (trriger == 0) {
+			return chack;
+		}
+		else {
+			for (int i = 32; i < 127; i++) {
+				trriger = 1;
+				for (int j = 0; j < bl.size(); j++) {
+					if (bl[j] == i) {
+						trriger = 0;
+						break;
+					}
+				}
+				if (trriger == 1) {
+					return i;
+				}
+
+			}
+
+		}
+		
+		
+		
+		
+		/*int result;
+		for (int i = 32; i < 127;i++) {
+			if (bl.find(i)) {
+
+			}
+			else {
+				result = i;
+			}
+		}
+
+		return result;*/
 	}
 public:
 	void key_generator(string path_alph, string file_name, string path_save_file) {
@@ -35,8 +83,85 @@ public:
 		if (FileIsExist(path_alph)) {
 			trriger = 1;
 		}
+		if (trriger == 0) {
+			ifstream alph_file(path_alph);
+			json alph;
+			alph_file >> alph;
+			alph_file.close();
+			auto array_alph = alph.find("alp");			
+			if (array_alph != alph.end()) { // если файл правильный
+				json key = { {"alg_type", "replacement"},{"key",{}} };
+				//key.at("key").push_back(json::array({ "w","f" }));
+				//key.at("key").push_back("ds");
+				//
+				//cout << key;
+				string alph_data_str;
+				json alph_data = alph.at("alp");
+				
+				json::iterator it = alph_data.begin();
+				for (int i = 0; i < alph.at("alp").size(); i++) { // запись данных библиотеки в стринг
+					string tt = *it;
+					alph_data_str.push_back(tt[0]);
+					it++;	
+				}
+				string key_data;
+				string black_list = "";
+				srand(time(0));
+				for (int i = 0; i < alph_data_str.size(); i++) {
+					int key_int = rand() % 20;
+					int minus = key_int % 2;
+					if (minus == 0) {
+						int chek = alph_data_str[i] - key_int;
+						int res = black_list_check(black_list, chek);
+						key_data.push_back(res);
+						black_list.push_back(res);
+						/*if (black_list.find(chek) == false) {
+							int result = black_list_check(black_list);
+							key_data.push_back(result);
+							black_list.push_back(result);
+
+						}
+						else {
+							key_data.push_back(alph_data_str[i] - key_int);
+							black_list.push_back(alph_data_str[i] - key_int);
+						}*/
+					}
+					else {
+						int chek = alph_data_str[i] + key_int;
+						int res = black_list_check(black_list, chek);
+						key_data.push_back(res);
+						black_list.push_back(res);
+						/*if (black_list.find(chek)==false|| chek > 126) {
+							int result = black_list_check(black_list);
+							key_data.push_back(result);
+							black_list.push_back(result);
+
+						}
+						else {
+							key_data.push_back(alph_data_str[i] - key_int);
+							black_list.push_back(alph_data_str[i] - key_int);
+						}*/
+
+					}
+				}
+				cout << key_data;
 
 
+
+
+
+
+
+
+			}
+			else {
+				cout << "alph is not correct!" << endl;
+			}
+
+		}
+		else {
+			cout << "ERROR" << endl;
+		}
 	}
 
 
